dd4eaa7f3b1c361a133bd7259c78483726e04b50
Zendelxy <410935054@gms.ndhu.edu.tw>
2023-08-26 20:58:42 +0800

    我忘記了其實user.java檔並不需要修改，然後我將tflite模型匯入並修改完了photo.java檔。順帶一提，我不確定你之前的程式碼還有沒有要用，所以我只將它們都註解化。

--- app/src/main/java/com/example/project5/photo.java
+++ app/src/main/java/com/example/project5/photo.java
@@ -6,14 +6,22 @@ import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
+import android.media.ThumbnailUtils;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.example.project5.ml.Model;
+
+import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
 
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
@@ -27,12 +35,20 @@ import com.bumptech.glide.Glide;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class photo extends AppCompatActivity {
+    Button btHigh = findViewById(R.id.buttonHigh);
+    TextView result = findViewById(R.id.result);
+    ImageView imageHigh = findViewById(R.id.imageViewHigh);
 
+    int imageSize = 224;
+
+    /*
     public static final String TAG = MainActivity.class.getSimpleName()+"My";
 
     private String mPath = "";//設置高畫質的照片位址
@@ -40,15 +56,27 @@ public class photo extends AppCompatActivity {
     public static final int CAMERA_PERMISSION = 100;//檢測相機權限用
     public static final int REQUEST_HIGH_IMAGE = 101;//檢測高畫質相機回傳
     private ActivityResultLauncher<Uri> launcher;
+    */
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.photo);
-        Button btHigh = findViewById(R.id.buttonHigh);
 
-
+        btHigh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Launch camera if we have permission
+                if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                    Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(cameraIntent, 1);
+                } else {
+                    //Request camera permission if we don't have it.
+                    requestPermissions(new String[]{Manifest.permission.CAMERA}, 100);
+                }
+            }
+        });
 
         ImageView home = findViewById(R.id.home);
         home.setOnClickListener(v -> {
@@ -56,7 +84,11 @@ public class photo extends AppCompatActivity {
             intent.setClass(photo.this, home.class);//目前Activity與目標Activity
             startActivity(intent);
         });
+
         /*取得相機權限*/
+
+        /*
+
         if (checkSelfPermission(Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED)
             requestPermissions(new String[]{Manifest.permission.CAMERA},CAMERA_PERMISSION);
         launcher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
@@ -67,6 +99,9 @@ public class photo extends AppCompatActivity {
             }
         });
         /*按下高畫質照相之拍攝按鈕*/
+
+        /*
+
         btHigh.setOnClickListener(v->{
             Intent highIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
             //檢查是否已取得權限
@@ -83,6 +118,7 @@ public class photo extends AppCompatActivity {
             highIntent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);
             launcher.launch(imageUri);//開啟相機
         });
+
     }
 
     private ActivityResultLauncher<Uri> registerForActivityResult(ActivityResultContracts.StartActivityForResult startActivityForResult, ActivityResultCallback<ActivityResult> callback) {
@@ -90,6 +126,8 @@ public class photo extends AppCompatActivity {
     }
 
     /**取得相片檔案的URI位址及設定檔案名稱*/
+
+        /*
     private File getImageFile()  {
         String time = new SimpleDateFormat("yyMMdd").format(new Date());
         String fileName = time+"_";
@@ -105,12 +143,21 @@ public class photo extends AppCompatActivity {
         }
     }
     /**取得照片回傳*/
+
+        /*
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         /*可在此檢視回傳為哪個相片，requestCode為上述自定義，resultCode為-1就是有拍照，0則是使用者沒拍照*/
+
+        /*
+
         Log.d(TAG, "onActivityResult: requestCode: " + requestCode + ", resultCode " + resultCode);
         /*如果是高畫質的相片回傳*/
+
+        /*
+
         if (requestCode == REQUEST_HIGH_IMAGE && resultCode == -1) {
             ImageView imageHigh = findViewById(R.id.imageViewHigh);
             new Thread(() -> {
@@ -134,5 +181,77 @@ public class photo extends AppCompatActivity {
         } else {
             Toast.makeText(this, "未作任何拍攝", Toast.LENGTH_SHORT).show();
         }
+        */
+    }
+
+    public void classifyImage(Bitmap image){
+        try {
+            Model model = Model.newInstance(getApplicationContext());
+
+            // Creates inputs for reference.
+            TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 224, 224, 3}, DataType.FLOAT32);
+            ByteBuffer bytebuffer =ByteBuffer.allocate(4 * imageSize*imageSize*3);
+            bytebuffer.order(ByteOrder.nativeOrder());
+
+            int[] intvalues = new int[imageSize * imageSize];
+            image.getPixels(intvalues,0,image.getWidth(),0,0,image.getWidth(),image.getHeight());
+            int pixel = 0;
+            for(int i=0;i<imageSize;i++){
+                for(int j=0;j<imageSize;j++) {
+                    int val =intvalues[pixel++];//RGB
+                    bytebuffer.putFloat(((val >>16)&0xff)*(1.f /255.f));
+                    bytebuffer.putFloat(((val >>8)&0xff)*(1.f /255.f));
+                    bytebuffer.putFloat((val & 0xff)*(1.f /255.f));
+
+                }
+            }
+
+
+
+            inputFeature0.loadBuffer(bytebuffer);
+
+            // Runs model inference and gets result.
+            Model.Outputs outputs = model.process(inputFeature0);
+            TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
+
+            float[]confidences= outputFeature0.getFloatArray();
+            int maxPos =0;
+            float maxConfidence = 0;
+            for(int i=0;i<confidences.length;i++){
+                if(confidences[i]>maxConfidence){
+                    maxConfidence =confidences[i];
+                    maxPos =i;
+                }
+            }
+            String[] classes ={"正常葉片","稻熱病","胡麻葉枯病","白葉枯病","褐調葉枯病"};
+
+            String s = "";
+            for (int i = 0;i<classes.length;i++) {
+                if (classes[maxPos] == classes[i])
+                {
+                    s = String.format("%s: %.1f%%\n", classes[i], confidences[i] * 100);
+                }
+            }
+            result.setText("辨識結果: \n"+String.format("%s(%.1f%%)\n", classes[maxPos], confidences[maxPos] * 100));
+
+            // Releases model resources if no longer used.
+            model.close();
+        } catch (IOException e) {
+            // TODO Handle the exception
+        }
+
+
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            Bitmap image=(Bitmap) data.getExtras().get("data");
+            int dimension = Math.min(image.getWidth(),image.getHeight());
+            image = ThumbnailUtils.extractThumbnail(image,dimension,dimension);
+            imageHigh.setImageBitmap(image);
+            image = Bitmap.createScaledBitmap(image,imageSize,imageSize,false);
+            classifyImage(image);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
